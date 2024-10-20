84662c72fb1216b81b682ecb700b8567ad9c2d46
kemwealth <richardgrey2158@gmail.com>
2024-10-20 14:21:53 -0400

    implemeting bubble sort function

--- bubble.js
+++ bubble.js
@@ -1,3 +1,20 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+    let numSwaps;
+    for (let i = array.length; i > 0; i--) {
+        numSwaps = false; // Initialize as false at the beginning of each iteration
+        for (let j = 0; j < i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap the elements
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                numSwaps = true; // Set to true if a swap occurred
+            }
+        }
+        // If no elements were swapped, the array is sorted
+        if (!numSwaps) break;
+    }
+    return array;
+}
 
 module.exports = bubbleSort;
